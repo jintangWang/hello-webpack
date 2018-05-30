e22b87308c7420784a344003d754cb63ed200031
maomao <LCX6916356@163.com>
2018-05-30 23:18:47 +0800

    添加stylus-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,23 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|styl)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'stylus-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
   }
 };
